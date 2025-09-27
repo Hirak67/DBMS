b4a02b74954943911e860d4ca339a9de7642ac82
Hirak Jyoti Sarma Basistha <sarmahirakjyoti796@gmail.com>
2025-09-28 02:39:37 +0530

    Add files via upload

---
+++ nlq_to_sql.py
@@ -0,0 +1,43 @@
+def nlq_to_sql(nlq):
+    nlq = nlq.lower()
+
+    # Marks sorting
+    if "marks" in nlq:
+        if "desc" in nlq or "descending" in nlq or "highest" in nlq:
+            return "SELECT * FROM students ORDER BY marks DESC;"
+        elif "asc" in nlq or "ascending" in nlq or "lowest" in nlq:
+            return "SELECT * FROM students ORDER BY marks ASC;"
+
+    # Name sorting(Alphabetical)
+    if "name" in nlq:
+        if "desc" in nlq or "reverse" in nlq or "z to a" in nlq:
+            return "SELECT * FROM students ORDER BY name DESC;"
+        else:
+            return "SELECT * FROM students ORDER BY name ASC;"
+
+    # Age sorting
+    if "age" in nlq:
+        if "youngest" in nlq or "asc" in nlq or "ascending" in nlq:
+            return "SELECT * FROM students ORDER BY age ASC;"
+        elif "oldest" in nlq or "desc" in nlq or "descending" in nlq:
+            return "SELECT * FROM students ORDER BY age DESC;"
+
+    # Multiple columns
+    if "column1" in nlq and "column2" in nlq:
+        # Determine column1 order
+        if "column1 in desc" in nlq or "column1 in descending" in nlq:
+            col1_order = "DESC"
+        else:
+            col1_order = "ASC"
+
+        # Determine column2 order
+        if "column2 in desc" in nlq or "column2 in descending" in nlq:
+            col2_order = "DESC"
+        else:
+            col2_order = "ASC"
+
+        return f"SELECT * FROM students ORDER BY column1 {col1_order}, column2 {col2_order};"
+
+    return "Wrong query"
+myinput= input("Enter your query in plain English: ")
+print("SQL:", nlq_to_sql(myinput))
